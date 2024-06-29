0adb3a68529b54f30785ec75630bbab9e2352e11
1NTORT <97185244+1ntort@users.noreply.github.com>
2024-06-29 08:49:37 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+local owner = "1ntort"
+local repository = "Rolex-Hub"
+local branch = "v2"
+
+local function Import(file)
+    return game:HttpGetAsync(("https://raw.githubusercontent.com/%s/%s/%s/%s.lua"):format(owner, repository, branch, file)), file .. '.lua')()
+end
+
+local main = webImport("bin/lib/main")
